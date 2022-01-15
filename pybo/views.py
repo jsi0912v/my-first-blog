631a29e43ebdd1dd37e418cfaaaab9b2ca0e8bee
jsi0912v <jsi0912v@gmail.com>
2022-01-15 09:03:22 +0900

    pybo paginator

--- pybo/views.py
+++ pybo/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index(request):
+    
+    #페이지를 얻어옴
+    page = request.GET.get('page', '1') 
+    
+    #조회
     question_list = Question.objects.order_by('-create_date')
-    context = { "question_list" : question_list }
+    
+    #페이지 처리
+    paginator = Paginator(question_list, 10) #페이지당 10개씩 보여주기.
+    page_obj = paginator.get_page(page)
+    
+    context = { "question_list" : page_obj }
     
     return render(request, 'pybo/question_list.html', context)
 
