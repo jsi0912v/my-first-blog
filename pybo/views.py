65ef4716ff5b65fca109985e22b168cd3e61cd4f
jsi0912v <jsi0912v@gmail.com>
2022-01-17 20:58:25 +0900

    add author in Question, Answer models

--- pybo/views.py
+++ pybo/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -29,12 +29,14 @@ def detail(request, question_id):
     
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date=timezone.now()
             answer.question = question
             answer.save()
@@ -45,8 +47,8 @@ def answer_create(request, question_id):
     return render(request, 'pybo/question_detail.html', context )
     
     
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     '''
+
     아래와 같이 변경하여 사용할 수 있다.
     
         from .models import Answer
@@ -55,15 +57,15 @@ def answer_create(request, question_id):
         answer.save()
     
     '''
-    
-    return redirect('pybo:detail', question_id=question.id)
 
+@login_required(login_url='common:login')
 def question_create(request):
     
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:detail', question_id=question.id)
