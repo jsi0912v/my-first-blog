1b02606deb1b9c83541a2822adb4bdf10d5811fb
jsi0912v <jsi0912v@gmail.com>
2022-01-20 20:54:14 +0900

    Question, Answer do comment

--- pybo/models.py
+++ pybo/models.py
@@ -10,8 +10,6 @@ class Question(models.Model):
     
     def __str__(self):
         return self.subject
-
-
 class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,3 +19,11 @@ class Answer(models.Model):
     
     def __str__(self):
         return self.question.id, self.content
+    
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
