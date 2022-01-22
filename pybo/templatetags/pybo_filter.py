de5167fed90b82b1148618b4533ba9babb86c7a9
jsi0912v <jsi0912v@gmail.com>
2022-01-22 17:09:53 +0900

    search 기능 작성 중 javsscript 부분 오류...

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,6 @@
+
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from ..models import Question
 
@@ -8,14 +9,25 @@ def index(request):
     #페이지를 얻어옴
     page = request.GET.get('page', '1') 
     
+    # 검색어를 얻어옴
+    kw = request.GET.get('kw', '') 
+    
     #조회
     question_list = Question.objects.order_by('-create_date')
+    
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
     
     #페이지 처리
     paginator = Paginator(question_list, 10) #페이지당 10개씩 보여주기.
     page_obj = paginator.get_page(page)
     
-    context = { "question_list" : page_obj }
+    context = { "question_list" : page_obj, 'page': page, 'kw': kw}
     
     return render(request, 'pybo/question_list.html', context)
 
